cdf5b3bf4fc58840ccf37a65dbabe76af64af720
GNOGz <99389733+GNOGz@users.noreply.github.com>
2025-12-30 03:42:56 +0700

    prisma work with database

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import { Elysia } from "elysia";
+import { PrismaClient } from "../generated/prisma/client";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const prisma = new PrismaClient();
+
+
+
+const app = new Elysia().get("/", async() => {
+  const course = await prisma.course.findMany();
+  return course;
+}).listen(3000);
+
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
