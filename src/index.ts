35ec209bc5a55548d00632459f50c3a1c5cd2fb2
GNOGz <99389733+GNOGz@users.noreply.github.com>
2026-01-07 23:56:19 +0700

    cors allow frontend to connect api

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,15 @@ import { Elysia } from "elysia";
 import { auth } from "./modules/auth";
 import { allCourse } from "./modules/alL_course";
 import {swagger} from '@elysiajs/swagger'
+import cors from "@elysiajs/cors";
 const app = new Elysia().get("/", async() => {
   return "Hello Elysia";
 })
+.use(cors({
+  origin:true,
+  credentials:true,
+  allowedHeaders: ['Content-Type', 'Authorization']
+}))
 .use(auth)
 .use(allCourse)
 .use(swagger())
