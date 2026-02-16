dff3d1460862a27e9e27c4bb2248d171e8898f0e
navyney <141761309+navyney@users.noreply.github.com>
2026-02-16 20:18:04 +0700

    remove cookie token checked

--- src/modules/curriculum/index.ts
+++ src/modules/curriculum/index.ts
@@ -2,34 +2,34 @@ import { Elysia } from "elysia";
 import { listCurriculums } from "./services/getListCurriculums";
 import { getRequiredCourseList } from "./services/getRequiredCourseList";
 
-function requireAdminToken(cookie: any, headers: any) {
-  const cookieToken = cookie["cmu-entraid-example-token"]?.value;
+// function requireAdminToken(cookie: any, headers: any) {
+//   const cookieToken = cookie["cmu-entraid-example-token"]?.value;
 
-  const devToken =
-    process.env.NODE_ENV !== "production"
-      ? headers["x-dev-token"]
-      : undefined;
+//   const testToken =
+//     process.env.NODE_ENV !== "production"
+//       ? headers["x-dev-token"]
+//       : undefined;
 
-  return cookieToken ?? devToken;
-}
+//   return cookieToken ?? testToken;
+// }
 
 export const curriculumModule = new Elysia({ prefix: "/admin" })
   .get("/curriculum", async ({ cookie, set , headers }) => {
-    const token = requireAdminToken(cookie, headers);
-    if (!token) {
-        set.status = 401;
-        return { message: "Unauthorized" };
-    }
+    // const token = requireAdminToken(cookie, headers);
+    // if (!token) {
+    //     set.status = 401;
+    //     return { message: "Unauthorized" };
+    // }
 
     return await listCurriculums();
   })
 
   .get("/curriculum/:program/:curriculum_year/courses", async ({ params, cookie, headers, set }) => {
-    const token = requireAdminToken(cookie, headers);
-    if (!token) {
-        set.status = 401;
-        return { message: "Unauthorized" };
-    }
+    // const token = requireAdminToken(cookie, headers);
+    // if (!token) {
+    //     set.status = 401;
+    //     return { message: "Unauthorized" };
+    // }
 
     const program = String(params.program).toUpperCase();
     if (program !== "CPE" && program !== "ISNE") {
