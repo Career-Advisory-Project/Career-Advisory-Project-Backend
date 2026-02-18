e0b84b578ee2fa573ed7ba671a0afe01e1e24e38
GNOGz <99389733+GNOGz@users.noreply.github.com>
2026-02-19 02:43:10 +0700

    done get and post for dashboard

--- src/modules/dashboard/index.ts
+++ src/modules/dashboard/index.ts
@@ -1,8 +1,57 @@
 import Elysia from "elysia"
 import { Dashboard } from "./service"
+import { DashboardModel } from "./model"
+import {t} from "elysia"
 export const dashboardRoute = new Elysia()
-.get("/dashboard/:cmuitaccount",({params})=>{
+.get("/dashboard/:cmuitaccount",async ({params,set})=>{
     const accountEmail = params.cmuitaccount;
-    const finishedCourses = Dashboard.getFinishedCourse(accountEmail) 
-    const unfinishedCoures = Dashboard.getUnfinishedCourse(accountEmail)
+    const courses = await Dashboard.getDashboardCourse(accountEmail) 
+    if(!courses){
+        set.status = 404
+        return "User not found"
+    }
+    console.log(courses)
+    return {
+        cmuitaccount:accountEmail,
+        courses:courses,
+    }
+})
+.post("/dashboard",async ({body,set})=>{
+    try{
+        await Dashboard.addCourseToDashboard({cmuitaccount:body.cmuitaccount,courseNoList:body.coursesNoList})
+        set.status = 200;
+        return {
+            ok:true,
+            message: "courses added to dashboard"
+        }
+    }
+    catch(error:unknown){
+        if(error instanceof DashboardModel.CourseNotFoundError){
+            set.status = 404
+            return{
+                ok:false,
+                message: "course not found: "+error.NotFoundList
+            }
+        }
+        else if(error instanceof Error){
+            set.status = 400
+            return{
+                ok:false,
+                message: error.message
+            }
+        }
+        else{
+            set.status = 500
+            return{
+                ok:false,
+                message:"Internal server error"
+            }
+        }
+        }
+   
+},{
+    body:t.Object({
+        cmuitaccount:t.String(),
+        coursesNoList:t.Array(t.String())
+    })
 })
--- src/modules/dashboard/service.ts
+++ src/modules/dashboard/service.ts
@@ -1,11 +1,129 @@
-
+import { Prisma } from "../../../generated/prisma/browser";
+import prisma from "../../db";
 import { DashboardModel } from "./model"
-export class Dashboard{
-    public static getFinishedCourse =  async (email:string)=>{
-       
+
+
+export class Dashboard {
+
+    public static getDashboardCourse = async (cmuitaccount: string): Promise<DashboardModel.courseListType | null> => {
+        const dashboardUser = await prisma.dashboard.findFirst({
+            where:{
+                cmuitaccount:cmuitaccount
+            }
+        })
+        if(!dashboardUser) return null
+        // console.log(dashboardUser.coursesNoList)
+        return dashboardUser.coursesNoList
     }
 
-    public static getUnfinishedCourse = async (email:string)=>{
+    private static mergeAndRemoveDuplicates = (
+        list1: DashboardModel.courseSchemaType[],
+        list2: DashboardModel.courseSchemaType[]
+    ): DashboardModel.courseSchemaType[] => {
+
+
+        const uniqueMap = new Map<string, DashboardModel.courseSchemaType>();
+
+        list1.forEach(item => {
+            uniqueMap.set(item.courseNo, item);
+        });
+
+
+        list2.forEach(item => {
+            uniqueMap.set(item.courseNo, item);
+        });
+
+        return Array.from(uniqueMap.values());
+    }
+
+    private static getDifference = (list1: string[], list2: string[]): string[] => {
+        return list1.filter(item => !list2.includes(item));
+    }
+    private static getCourseListToAdd = async (courseData: DashboardModel.courseSchemaType[], courseNoList: string[]): Promise<DashboardModel.courseSchemaType[] | null> => {
+        const existCourseItem = courseData
+        const existCourseNoList = existCourseItem.map((item) => {
+            return item.courseNo
+        })
+        const existCourseNoItems = courseData
+
+        const courseToAdd = await prisma.course.findMany({
+            where: {
+                courseNo: {
+                    in: courseNoList
+                }
+            }
+        })
+        if(courseNoList.length !== courseToAdd.length){
+            const courseToAddNo = courseToAdd.map((item)=>{
+                return item.courseNo
+            });
+            const courseNotFound = this.getDifference(courseNoList,courseToAddNo);
+            const exception = new DashboardModel.CourseNotFoundError("Course not found",courseNotFound);
+            throw exception;
+        }
+        const courseToAddData: DashboardModel.courseSchemaType[] = courseToAdd.map((courseItem) => {
+            return {
+                courseNo: courseItem.courseNo,
+                name: courseItem.name
+            }
+        })
+        this.mergeAndRemoveDuplicates(existCourseNoItems, courseToAddData)
+
+        return this.mergeAndRemoveDuplicates(existCourseNoItems, courseToAddData)
 
     }
+    public static addCourseToDashboard = async ({ cmuitaccount, courseNoList }: DashboardModel.courseBodyParamType) => {
+        const userToFind = await prisma.dashboard.findFirst({
+            where: {
+                cmuitaccount: cmuitaccount
+            }
+        })
+
+        if (userToFind) { // courseDashboard already exist
+
+            try {
+                const finalCourseList = await this.getCourseListToAdd(userToFind.coursesNoList, courseNoList)
+                if (!finalCourseList) return
+                const courseAdded = await prisma.dashboard.update({
+                    where: {
+                        cmuitaccount: cmuitaccount
+                    },
+                    data: {
+                        cmuitaccount: cmuitaccount,
+                        coursesNoList: finalCourseList
+                    },
+                })
+            }
+            catch(error:any){
+                throw error
+            }
+        }
+        else {
+            const courseToAdd = await prisma.course.findMany({
+                where: {
+                    courseNo: {
+                        in: courseNoList
+                    }
+                }
+            })
+            const courseItemList: DashboardModel.courseSchemaType[] = courseToAdd.map((item) => {
+                return {
+                    courseNo: item.courseNo,
+                    name: item.name
+                }
+            })
+
+            await prisma.dashboard.create({
+                data: {
+                    cmuitaccount: cmuitaccount,
+                    coursesNoList: courseItemList
+                }
+            })
+        }
+    }
+
+    public static removeCouseFromDashboard = async ({ cmuitaccount, courseNoList }: DashboardModel.courseBodyParamType) => {
+
+    }
+
 }
