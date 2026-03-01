5c087d8b41d3c24a6047a305579392d99f53ae6c
GNOGz <99389733+GNOGz@users.noreply.github.com>
2026-03-02 02:29:56 +0700

    add authorization to all api

--- src/modules/validator/index.ts
+++ src/modules/validator/index.ts
@@ -27,5 +27,44 @@ export const validateUser = async (context: Context) => {
             message: "Unauthorized: Token signature invalid"
         }        
     }
+
+    if(!profile.role){
+        set.status = 401
+        return{
+            ok:false,
+            message:"User is not unauthorized"
+        }
+    }
     
+}
+
+export const validateAdmin = async (context: Context) =>{
+const { jwt, cookie: { "cmu-entraid-example-token": cmuToken }, set } = context as AuthContext;
+
+
+    if (!cmuToken?.value) {
+        set.status = 401
+        return {
+            ok: false,
+            message: "Unauthorized: Cookie not found"
+        }
+    }
+
+    const profile = await jwt.verify(cmuToken.value as string)
+
+    if (!profile) {
+        set.status = 401
+        return {
+            ok: false,
+            message: "Unauthorized: Token signature invalid"
+        }        
+    }
+
+    if(!profile.role || profile.role != "admin"){
+        set.status = 401
+        return{
+            ok:false,
+            message:"User is not unauthorized"
+        }
+    }
 }
