73eee52d4e875716d4d228d1fd27bff9a1675558
GNOGz <99389733+GNOGz@users.noreply.github.com>
2026-03-04 15:31:08 +0700

    inntial test unit 2

--- test/app.test.ts
+++ test/app.test.ts
@@ -6,7 +6,6 @@ const api = treaty(app)
 
 describe('Elysia routes', () => {
     it('GET /dashboard get dashboard course for teacher', async () => {
-        // Fix: Call dashboard as a function to satisfy the path parameter, THEN call .get()
         const { data, error } = await api.dashboard({ 
             cmuitaccount: "yutthakarn_sajui@cmu.ac.th" 
         }).get();
